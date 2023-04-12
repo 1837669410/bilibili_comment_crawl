63f1f2a75d7a0ce6e92949297c9bbe8cef0b1c55
zhangtianwei <1837669410@qq.com>
2023-04-12 15:50:26 +0800

    更新爬虫停止策略

--- crawl.py
+++ crawl.py
@@ -11,15 +11,19 @@ header = {
 }
 
 comment = []
-for i in range(100):
+pre_comment_length = 0
+
+for i in range(200):
     responses = requests.get(url=url.format(i), headers=header).json()
     for content in responses["data"]["replies"]:
         comment.append(content["content"]["message"])
-    if len(comment) % 20 != 0:
-        print("一共爬取到{}条评论".format(len(comment)))
+    print("搜集到%d条评论" % (len(comment)))
+    # 调整爬虫策略，从必须每20条评论调整成上一次评论数和这一次评论数进行比较，如果有改变说明有新数据，如果没改变说明数据全部搜集完毕，爬虫停止
+    if len(comment) == pre_comment_length:
+        print("爬虫退出！！！")
         break
     else:
-        print("搜集到%d条评论" % (len(comment)))
+        pre_comment_length = len(comment)
 
 with open("bilibili_comment.txt", "w", encoding="utf-8") as fp:
     for c in comment:
